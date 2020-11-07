49db87a14de5e8d2dda98fa4d057a11f6493e138
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2020-11-07 20:35:57 +0800

    Add files via upload

---
+++ p141-6.18(3).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d=1,e;
+	for(a=1;a<=9;a++)
+	{
+		printf("%2d ",a);
+	}
+	printf("\n");
+    for(b=1;b<=9;b++)
+	{
+		printf(" - ");
+	}
+	printf("\n");
+		for (c=1;c<=9;c++)          
+	{
+		for (d=1;d<=3*c-3; d++) 
+		{
+			printf(" ");          
+		}
+		for (e=c;e<=9;e++)      
+		{
+			printf("%2d ",c*e);    
+		}
+		printf("\n");               
+	}
+	return 0;
+}
