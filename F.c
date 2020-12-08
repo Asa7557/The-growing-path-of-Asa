1458f876b1d80684d2be1a29cecaf98249d600cd
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2020-12-08 22:14:28 +0800

    Add files via upload

---
+++ F.c
@@ -0,0 +1,17 @@
+#include"stdio.h"
+int main()
+{
+	double s=1,y=0;
+    int x=1,i=0;
+	for(x=1;x<=30;x++)
+	{
+		s=1;
+		for(i=1;i<=x;i++)
+		{
+			s=s*i;
+		}
+		y=y+s;
+	}
+	printf("%.2e",y);
+	return 0;
+}
