33625a41aa182b65eb16d931eb0511a58ef1f7a7
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2020-12-10 19:02:16 +0800

    Add files via upload

---
+++ H.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,q=0,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+    	if(a[i]%2==0)
+        q=q+a[i];
+	}
+	printf("%d",q);
+	return 0;
+}
