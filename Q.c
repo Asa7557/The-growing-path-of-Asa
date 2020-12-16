1dfac0dfb28fa7a38db768e8887edf81d59afa03
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2020-12-16 13:40:16 +0800

    Add files via upload

---
+++ Q.c
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[6],g[8],i,o,c,b=0,d;
+	for(i=0;i<6;i++) scanf("%d",&a[i]);
+	for(o=0;o<8;o++) scanf("%d",&g[o]);
+	sort(a,a+6);
+	for(i=0;i<6;i++)
+	{
+		for(o=0;o<8;o++)
+		{
+			if(a[i]==g[o])
+			{
+				printf("%d\n",a[i]);
+				break;
+			}
+		}
+	}
+	return 0;
+}
