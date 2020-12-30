daff92a7acd195c5e419849f2088d8a40ef60100
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2020-12-30 21:08:13 +0800

    Update 素数.cpp

--- 素数.cpp
+++ 素数.cpp
@@ -6,12 +6,10 @@ int main()
     scanf("%d", &n);
     for (i = 2; i < n ; i++)
     {
-        if (n%i == 0)
-            break;
+        if (n%i == 0) break;
     }
-    if (n <=1 ) printf("This is not a prime\n");
-    else  if (i < n) printf("This is not a prime.\n"); 
-    else printf("This is a prime.\n");
+    if (i==n) printf("This is a prime\n");
+    else printf("This is not a prime.\n");
     return 0;
  
 }
