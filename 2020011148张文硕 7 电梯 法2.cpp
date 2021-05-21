1dcc40f0bbbf64bfde686c245e77ecfc6c9ac91a
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2021-05-21 09:38:43 +0800

    Add files via upload

---
+++ 2020011148张文硕 7 电梯 法2.cpp
@@ -0,0 +1,30 @@
+/*2020011165ÖÇ¼Îè¯
+µçÌİ*/
+#include<stdio.h>
+#define N 10000
+int main()
+{
+	int i=1,m,n,flag[N],t=0;
+	flag[0]=0;
+	scanf("%d",&n);
+    while(n)
+    {
+	while(n--)
+	{   
+		scanf("%d",&m);
+		flag[i]=m;
+		if(flag[i]>flag[i-1])
+		{
+			t=t+6*(flag[i]-flag[i-1])+5;
+		}
+		if(flag[i]<flag[i-1])
+		{
+			t=t+4*(flag[i-1]-flag[i])+5;
+		}
+		i=i+1;	
+   } 
+   	printf("%d\n",t);
+    scanf("%d",&n);
+   }
+	return 0;
+}
