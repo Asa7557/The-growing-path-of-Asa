aed777e779e5dea15ab35ace3447d4d42ac6e01c
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2021-06-17 19:45:18 +0800

    Rename 2020011148张文硕 7 电梯.cpp to 电梯.cpp

--- 电梯.cpp
+++ 电梯.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int calculate(int x)
 {
-	int memory,temp;//memory���ڴ�����һ�ε�xҲ���ǵ�ǰ¥�� 
+	int memory,temp;//memory用于储存上一次的x也就是当前楼层 
 	if(x>memory)  
 	{
 		temp=6*(x-memory)+5;
@@ -24,20 +24,20 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
-	while(n)//��ÿ�е�һ������ 0 ʱ���� 
+	while(n)//当每行第一个数是 0 时结束 
 	{	
 	    int m,sum=0;
-	    while(n--)//����ÿ����������ݸ��� 
+	    while(n--)//控制每行输入的数据个数 
 	    {
 		    scanf("%d",&m);
-		    if(sum==0)//��һ�γ�ʼ¥��Ϊ0 
+		    if(sum==0)//第一次初始楼层为0 
 		    {
 		    	calculate(m);
-				//���ú�������Ϊ�˰�¥��(m)���ݸ���������memory��ס���¥���´��� 
+				//引用函数仅仅为了把楼层(m)传递给函数并用memory记住这个楼层下次用 
 		    	sum=6*m+5;
 			}
 		    else sum=sum+calculate(m);
-			//�Եڶ��ο�ʼÿ�����ú���������������������ʱ�� 
+			//自第二次开始每次引用函数计算电梯这次运行所需时间 
 	    }
 	    printf("%d\n",sum);
 	    scanf("%d",&n);
