2394e56012c48f42ee1f4b83d7d7c259adfa8545
Asa7557 <74052672+Asa7557@users.noreply.github.com>
2021-07-02 21:20:03 +0800

    Rename 矩阵转置.cpp to 矩阵转置2.cpp

--- 矩阵转置2.cpp
+++ 矩阵转置2.cpp
@@ -4,14 +4,14 @@ int main()
 	int m,n,i,j;
 	scanf("%d %d",&m,&n);
 	int a[m][n];
-	for(i=0;i<m;i++)//ÊäÈë 
+	for(i=0;i<m;i++)//ÃŠÃ¤ÃˆÃ« 
 	{
 		for(j=0;j<n;j++)
 		{
 		    scanf("%d",&a[i][j]);
 		}
 	}
-	for(i=0;i<n;i++)//×ªÖÃÊä³ö 
+	for(i=0;i<n;i++)//Ã—ÂªÃ–ÃƒÃŠÃ¤Â³Ã¶ 
 	{
 		for(j=0;j<m;j++)
 		printf("%d ",a[j][i]);
